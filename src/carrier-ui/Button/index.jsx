8d93ee17824515c12bea6fa9e04d4aa8ae0d5c9f
challee <seoungin1228@gmail.com>
2021-08-02 01:21:02 +0900

    feat: add components and color

--- src/carrier-ui/Button/index.jsx
+++ src/carrier-ui/Button/index.jsx
@@ -1,3 +1,71 @@
-import styled from 'styled-components';
+import styled, { css, withTheme } from 'styled-components';
 
-export default styled.button``;
+const Button = styled.button`
+  position: relative;
+  height: 50px;
+  width: 250px;
+  border-radius: 10px;
+  font-size: 1rem;
+  background-color: ${props => props.theme.colors.primary};
+  z-index: inherit;
+
+  ${props =>
+    props.round &&
+    css`
+      border-radius: 24px;
+    `}
+
+  ${props =>
+    props.large &&
+    css`
+      width: 80px;
+      height: 350px;
+      font-size: 1.5rem;
+    `};
+
+  ${props =>
+    props.medium &&
+    css`
+      width: 50px;
+      height: 250px;
+      font-size: 1rem;
+    `};
+
+  ${props =>
+    props.small &&
+    css`
+      width: 35px;
+      height: 200px;
+      font-size: 0.75rem;
+    `};
+
+  ${props =>
+    props.primary &&
+    css`
+      background-color: ${props => props.theme.colors.primary};
+      color: ${props => props.theme.colors.primary};
+    `}
+
+  ${props =>
+    props.disabled &&
+    css`
+      background-color: ${props.primary
+        ? props.theme.colors.white
+        : props.theme.colors.disabled};
+      color: ${props.primary
+        ? props.theme.colors.gray400
+        : props.theme.colors.onDisabled};
+      border: 1px solid
+        ${props.primary ? props.theme.colors.gray400 : props.theme.colors.border};
+      cursor: default;
+    `}
+
+  ${props =>
+    props.color &&
+    css`
+      color: ${props.theme.colors[props.color]};
+    `}
+
+`;
+
+export default withTheme(Button);
--- src/carrier-ui/TextField/index.jsx
+++ src/carrier-ui/TextField/index.jsx
@@ -1,3 +1,105 @@
-import styled from 'styled-components';
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import styled, { withTheme } from 'styled-components';
 
-export default styled.input``;
+import { Typography, Margin } from 'carrier-ui';
+
+const Input = styled.input`
+  border: 1px solid ${props => props.theme.colors.black};
+  border-radius: 10px;
+  font-size: 1rem;
+  padding: 10px;
+  width: 300px;
+  height: 50px;
+  box-sizing: border-box;
+  outline: none;
+
+  &::placeholder {
+    color: ${props => props.theme.colors.gray};
+  }
+
+  &:focus {
+    border: 1px solid ;
+    border-color : black;
+  }
+
+  ${props =>
+    props.large &&
+    `
+      width: 450px;
+      height: 70px;
+      padding: 15px;
+    `}
+
+  ${props =>
+    props.medium &&
+    `
+    width: 300px;
+    height: 50px;
+  `}
+
+  ${props =>
+    props.small &&
+    `
+    width: 250px;
+    height: 40px;
+    padding: 8px;
+  `}
+
+  ${props =>
+    props.search &&
+    `
+    background-image: url(${searchIcon});
+    background-repeat: no-repeat;
+    background-position: 10px center;
+    padding: 10px 12px 10px 40px;
+    `}
+
+`;
+
+function TextField({
+  label,
+  value,
+  search,
+  disabled,
+  ...rest
+}) {
+  return (
+    <div>
+      {label && (
+        <Typography body2 color="black">
+          {label}
+        </Typography>
+      )}
+      <Input value={value} search={search} disabled={disabled} {...rest} />
+  
+    </div>
+  );
+}
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  success: PropTypes.string,
+  error: PropTypes.string,
+  comment: PropTypes.string,
+  validate: PropTypes.bool,
+  search: PropTypes.bool,
+  noMessage: PropTypes.bool,
+  noMargin: PropTypes.bool,
+};
+
+TextField.defaultProps = {
+  label: undefined,
+  value: '',
+  success: '',
+  error: '',
+  comment: '',
+  validate: undefined,
+  search: false,
+  noMessage: false,
+  noMargin: false,
+};
+
+export default withTheme(TextField);
