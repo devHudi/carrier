049a60434a08b7b78a6d4fdaea8b5c2871fc4b01
challee <seoungin1228@gmail.com>
2021-08-02 01:21:02 +0900

    feat: add components and color

---
+++ src/carrier-ui/Margin/index.jsx
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components';
+
+const Margin = styled.div`
+  height: calc(${props => props.size} * 1rem);
+
+  ${props =>
+    props.row &&
+    css`
+      height: 0;
+      width: calc(${props.size} * 1rem);
+    `}
+`;
+
+export default Margin;
