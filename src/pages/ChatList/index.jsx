375375c159819eb856d153cec35b841183952050
JunHyeon <dlwnsgus07@naver.com>
2021-08-07 03:56:28 +0900

    fix : fix the collection name

--- src/pages/ChatList/index.jsx
+++ src/pages/ChatList/index.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import PageSpinner from 'carrier-ui/PageSpinner';
 import { auth, firestore } from '../../misc/firebase';
 import Container from '../../carrier-ui/Container';
-import Navigation from './ Component/ChatListNav/style';
+import ChatNavigation from './ Component/ChatListNav/style';
 import Search from './ Component/SearchBar/search';
 import ChatRoom from './ Component/ChatRoom';
 
@@ -12,6 +14,7 @@ const ChatList = () => {
   const [user, setUser] = useState('');
   const [userObj, setUserObj] = useState(null);
   const [searchWord, setSearchWord] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   //* *********************로그인한 사용자로부터 유저 데이터 가져오는 곳************************//
 
   useEffect(async () => {
@@ -36,10 +39,10 @@ const ChatList = () => {
   }, [userObj]);
   //* ***********************************************************************************//
   // 밑에 useEffect 코드 리펙토링 할때 적은 코드로 사용할 수 있게 하기. + collection name chat->chats로 변경하기
-  useEffect(() => {
+  useEffect(async () => {
     if (user === 'employer') {
       firestore
-        .collection('chat')
+        .collection('chats')
         .where('employer_uid', '==', userObj?.uid)
         .onSnapshot((snapshot) => {
           setChatRoomList(
@@ -49,9 +52,10 @@ const ChatList = () => {
             })),
           );
         });
+      setIsLoading(false);
     } else if (user === 'employee') {
       firestore
-        .collection('chat')
+        .collection('chats')
         .where('employee_uid', '==', userObj?.uid)
         .onSnapshot((snapshot) => {
           setChatRoomList(
@@ -61,6 +65,7 @@ const ChatList = () => {
             })),
           );
         });
+      setIsLoading(false);
     }
   }, [user]);
   const onChange = (e) => {
@@ -72,11 +77,19 @@ const ChatList = () => {
   const onSubmit = () => {
     setSearchWord('');
   };
+  const history = useHistory();
+  const onLeftClick = () => {
+    history.goBack();
+  };
   return (
     <>
-      <Navigation leftIcon="back" rightIcon="more">
+      <ChatNavigation
+        leftIcon="back"
+        rightIcon="more"
+        onLeftIconClick={onLeftClick}
+      >
         메신저
-      </Navigation>
+      </ChatNavigation>
       <Container top={63}>
         <Search
           top={20}
@@ -84,6 +97,7 @@ const ChatList = () => {
           value={searchWord}
           onSubmit={onSubmit}
         />
+        {isLoading && <PageSpinner />}
         {chatRoomList.map((chat) =>
           classification ? (
             <ChatRoom classification={classification} userObj={chat} />
@@ -96,101 +110,3 @@ const ChatList = () => {
   );
 };
 export default ChatList;
-
-// snapshot.forEach((doc) => {
-//   chats.push(
-//     dbService
-//       .collection("chat")
-//       .doc(doc.id)
-//       .collection("conversation")
-//   );
-// });
-// console.log(chatRoomList);
-// chats.forEach((chat) => {
-//   chat.onSnapshot((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//       // doc.data() is never undefined for query doc snapshots
-//       console.log(doc.id, " => ", doc.data());
-//     });
-//   });
-// });
-
-// dbService
-//   .collection("chat")
-//   .where("employee_uid", "==", userObj.uid)
-//   .onSnapshot((snapshot) => {
-//     let chatRoom = [];
-//     snapshot.forEach((doc) => {
-//       chatRoom.push(doc.get("employee_uid")); // 상대방 사용자 정보 가져오기
-//       setChatRoomList(chatRoom);
-//       chats.push(
-//         dbService
-//           .collection("chat")
-//           .doc(doc.id)
-//           .collection("conversation")
-//       );
-//     });
-//     chats.forEach((chat) => {
-//       chat.onSnapshot((querySnapshot) => {
-//         querySnapshot.forEach((doc) => {
-//           // doc.data() is never undefined for query doc snapshots
-//           console.log(doc.id, " => ", doc.data());
-//         });
-//       });
-//     });
-//   });
-
-// chats[0].onSnapshot((snapshot) => {
-//   setConversation(
-//     snapshot.docs.map((chatDoc) => ({
-//       ...chatDoc.data(),
-//     }))
-//   );
-// });
-// useEffect(async () => {
-//   let chatCollection;
-//   if (user === "employer") {
-//     await dbService
-//       .collection("chat")
-//       .where("employer_uid", "==", userObj.uid)
-//       .get()
-//       .then((querySnapshot) => {
-//         querySnapshot.forEach((doc) => {
-//           chatCollection = dbService
-//             .collection("chat")
-//             .doc(doc.id)
-//             .collection("conversation");
-//         });
-//       });
-//     chatCollection.onSnapshot((snapshot) => {
-//       setConversation(
-//         snapshot.docs.map((chatDoc) => ({
-//           ...chatDoc.data(),
-//         }))
-//       );
-//       // console.log(conversation);
-//     });
-//   } else {
-//     dbService
-//       .collection("chats")
-//       .where("employee_uid", "==", userObj.uid)
-//       .onSnapshot((snapshot) => {
-//         setChatRoom(
-//           snapshot.docs.map((chatDoc) => ({
-//             id: chatDoc.id,
-//             ...chatDoc.data(),
-//           }))
-//         );
-//       });
-//   }
-// }, [user]);
-
-// useEffect(() => {
-//   chatRoomList.map((chat) => {
-//     firestore
-//       .collection('users')
-//       .where('uid', '==', chat.employee_uid)
-//       .get()
-//       .then((querySnapshot) => {});
-//   });
-// }, [chatRoomList]);
