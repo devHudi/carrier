75c635791a5e706e299a961ce10464e1d35fd4ac
JunHyeon <dlwnsgus07@naver.com>
2021-08-07 03:58:55 +0900

    chore : modify style-component

--- src/pages/ChatList/ Component/ChatListNav/style.jsx
+++ src/pages/ChatList/ Component/ChatListNav/style.jsx
@@ -1,66 +1,84 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { AiOutlineMinus, AiOutlineSearch } from 'react-icons/all';
+import { useHistory } from 'react-router-dom';
+import { MdChat } from 'react-icons/md';
+import { IoPersonSharp } from 'react-icons/io5';
+import { AiFillHome } from 'react-icons/ai';
+import { IoIosArrowBack, IoIosMore } from 'react-icons/io';
 
 const Wrapper = styled.div`
-  top: ${(props) => (props.top ? `${props.top}px` : 'initial')};
   width: 100%;
-  height: 48px;
-  padding: 14px 20px 14px 15px;
-  border: 2px solid #ffffff;
-  border-radius: 48px;
-  box-shadow: 3px 3px 7px rgb(0 0 0 / 0.1);
+  position: fixed;
+  top: 0;
+  color: ${(props) => props.theme.colors.white};
+  font-size: 20px;
+  line-height: 25px;
+  letter-spacing: 0px;
+  padding: 23px 24px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 28px;
 `;
+
 const Icon = styled.div`
   display: flex;
   align-items: center;
   font-size: 25px;
-  color: ${(props) => props.theme.colors.blue};
+  color: ${(props) => props.theme.colors.white};
   cursor: pointer;
-  &:first-child {
-    transform: rotate(90deg);
-  }
 `;
 
-const Input = styled.input`
-  text-align: left;
-  border-style: none;
-  width: 100%;
-  font-size: 19px;
-  &::placeholder {
-    font-family: NanumSquare sans-serif;
-    font-weight: normal;
-    font-size: 19px;
-    line-height: 40px;
-    letter-spacing: 0px;
-    color: ${(props) => props.theme.colors.blue};
+const createIcon = (history, type, onClick) => {
+  if (type === 'chat') {
+    return <MdChat onClick={onClick} />;
   }
-`;
-const Search = ({ top, value, onChangeWord, onSubmit }) => {
-  const placeholder = '검색';
+  if (type === 'mypage') {
+    return <IoPersonSharp onClick={onClick} />;
+  }
+  if (type === 'home') {
+    return <AiFillHome onClick={onClick} />;
+  }
+  if (type === 'back') {
+    return <IoIosArrowBack onClick={onClick} />;
+  }
+  if (type === 'more') {
+    return <IoIosMore onClick={onClick} />;
+  }
+  return null;
+};
+
+const ChatNavigation = ({
+  leftIcon,
+  rightIcon,
+  onLeftIconClick,
+  onRightIconClick,
+  children,
+}) => {
+  const history = useHistory();
+
   return (
-    <Wrapper top={top} onSubmit={onSubmit}>
-      <Icon>
-        <AiOutlineMinus />
-      </Icon>
-      <Input value={value} onChange={onChangeWord} placeholder={placeholder} />
-      <Icon>
-        <AiOutlineSearch onClick={onSubmit} />
-      </Icon>
+    <Wrapper>
+      <Icon>{createIcon(history, leftIcon, onLeftIconClick)}</Icon>
+      {children}
+      <Icon>{createIcon(history, rightIcon, onRightIconClick)}</Icon>
     </Wrapper>
   );
 };
-Search.propTypes = {
-  top: PropTypes.number,
-  value: PropTypes.string.isRequired,
-  onChangeWord: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+
+ChatNavigation.propTypes = {
+  leftIcon: PropTypes.oneOf(['chat', 'mypage', 'home', 'back', 'more']),
+  rightIcon: PropTypes.oneOf(['chat', 'mypage', 'home', 'back', 'more']),
+  onLeftIconClick: PropTypes.func,
+  onRightIconClick: PropTypes.func,
+  children: PropTypes.element,
 };
-Search.defaultProps = {
-  top: 0,
+
+ChatNavigation.defaultProps = {
+  leftIcon: 'chat',
+  rightIcon: 'mypage',
+  onLeftIconClick: () => {},
+  onRightIconClick: () => {},
+  children: <></>,
 };
-export default Search;
+
+export default ChatNavigation;
