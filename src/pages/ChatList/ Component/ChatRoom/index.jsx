deb75f4d499ed384028e1746fd3e3e1c7644a1bb
JunHyeon <dlwnsgus07@naver.com>
2021-08-07 03:58:24 +0900

    feat : get Image from firebase and fix bug

--- src/pages/ChatList/ Component/ChatRoom/index.jsx
+++ src/pages/ChatList/ Component/ChatRoom/index.jsx
@@ -1,17 +1,15 @@
 /* eslint-disable no-unused-vars */
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Img, MessageBox, RecentBox, Wrapper } from './style';
+import { Img, MessageBox, RecentBox, Wrapper, StyledLink } from './style';
 
 const ChatRoom = ({ userObj, classification }) => {
   const count = 2;
-  console.log(classification);
   return (
     <>
       {classification ? (
-        <Link
+        <StyledLink
           to={{
-            pathname: `/chat/${userObj.id}`,
+            pathname: `/chat/${userObj?.id}`,
             state: {
               userObj,
               classification,
@@ -19,21 +17,21 @@ const ChatRoom = ({ userObj, classification }) => {
           }}
         >
           <Wrapper>
-            <Img src="" alt="" />
+            <Img src={userObj?.employer_profile_img} alt="" />
             <MessageBox>
-              <span>{userObj.employer_name}</span>
+              <span>{userObj?.employer_name}</span>
               <span>안녕하세요 소비자 입니다~~~</span>
             </MessageBox>
             <RecentBox>
-              <span>{userObj.updated_at}</span>
+              <span>{userObj?.updated_at}</span>
               <div>{count}</div>
             </RecentBox>
           </Wrapper>
-        </Link>
+        </StyledLink>
       ) : (
-        <Link
+        <StyledLink
           to={{
-            pathname: `/chat/${userObj.id}`,
+            pathname: `/chat/${userObj?.id}`,
             state: {
               userObj,
               classification,
@@ -41,64 +39,24 @@ const ChatRoom = ({ userObj, classification }) => {
           }}
         >
           <Wrapper>
-            <Img src="" alt="" />
+            <Img src={userObj?.employee_profile_img} alt="" />
             <MessageBox>
-              <span>{userObj.employee_name}</span>
+              <span>{userObj?.employee_name}</span>
               <span>안녕하세요 가이드 입니다~~~</span>
             </MessageBox>
             <RecentBox>
-              <span>{userObj.updated_at}</span>
+              <span>{userObj?.updated_at}</span>
               <span>{count}</span>
             </RecentBox>
           </Wrapper>
-        </Link>
+        </StyledLink>
       )}
     </>
   );
 };
 
 ChatRoom.propTypes = {
-  userObj: PropTypes.arrayOf('any').isRequired,
+  userObj: PropTypes.arrayOf(PropTypes.object).isRequired,
   classification: PropTypes.bool.isRequired,
 };
 export default ChatRoom;
-// import PropTypes from 'prop-types';
-//
-// const ChatRoom = ({ userObj, classification }) => {
-//   const [recentConversation, setRecentConversation] = useState('');
-//   const [count, setCount] = useState(0);
-//   return (
-//     <>
-//       classification?(
-//       <div>
-//         <img src={userObj.employer_profile_img} alt="" />
-//         <div>
-//           <span>{userObj.employer_name}</span>
-//           <span>{recentConversation}</span>
-//         </div>
-//         <div>
-//           <span>{userObj.updated_at}</span>
-//           <div>{count}</div>
-//         </div>
-//       </div>
-//       ):(
-//       <div>
-//         <img src={userObj.employee_profile_img} alt="" />
-//         <div>
-//           <span>{userObj.employee_name}</span>
-//           <span>{recentConversation}</span>
-//         </div>
-//         <div>
-//           <span>{userObj.updated_at}</span>
-//           <div>{count}</div>
-//         </div>
-//       </div>
-//       )
-//     </>
-//   );
-// };
-// ChatRoom.propTypes = {
-//   userObj: PropTypes.arrayOf('any').isRequired,
-//   classification: PropTypes.bool.isRequired,
-// };
-// export default ChatRoom;
