2dc4bc809d8a6c66368d395a5e45877552b2a71a
devHudi <devhudi@gmail.com>
2021-08-09 06:59:46 +0900

    feat: implement WidthLimit

---
+++ src/carrier-ui/WidthLimit/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const WidthLimit = styled.div`
+  height: 100%;
+  margin: 0 auto;
+  max-width: 375px;
+  padding: 50px;
+`;
+
+export default WidthLimit;
