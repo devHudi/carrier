0e61b071c5d588f55ef2870ad67f6478987259ce
JunHyeon <dlwnsgus07@naver.com>
2021-08-08 22:47:59 +0900

    chore : fix styled_component

--- src/pages/Chat/components/ChatBox/style.jsx
+++ src/pages/Chat/components/ChatBox/style.jsx
@@ -2,76 +2,128 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import paper from './assets/paperplane.png';
 import plus from './assets/plus.png';
+import line from './assets/line.png';
 
-const Wrapper = styled.form`
-  height: 48px;
-  width: 90%;
-  position: absolute;
-  bottom: 27px;
-  padding: 14px 20px 14px 15px;
-  border: 2px solid #ffffff;
-  border-radius: 48px;
-  box-shadow: 3px 3px 7px rgb(0 0 0 / 0.1);
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 99px;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+const TextWrapper = styled.div`
+  width: 100%;
+  height: 55px;
+  padding-left: 10px;
+  padding-right: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 100%;
-  background-color: ${(props) => props.theme.colors.white};
+`;
+const Form = styled.form`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  height: 35px;
+  width: 71%;
+  bottom: 14px;
+  padding: 14px 20px 14px 15px;
+  border-radius: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eeefff;
 `;
 
 const MessageInput = styled.textarea`
   text-align: left;
   border-style: none;
-  margin-right: 20px;
-  width: 80%;
+  margin-right: 0px;
+  width: 90%;
   resize: none;
   font-size: 16px;
   background: none;
   border: none;
 `;
 const SubmitButton = styled.button`
-  position: absolute;
   z-index: 1;
-  right: 15px;
   border: none;
-  width: 30px;
-  height: 30px;
   outline: none !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   cursor: pointer;
   background-color: transparent;
-  margin-right: 0.7rem;
   & > img {
-    width: 25px;
-    height: 25px;
+    width: 19px;
+    height: 19px;
   }
   &:hover {
     opacity: 0.8;
   }
 `;
 const Plus = styled.img`
-  width: 20px;
+  width: 19px;
+  height: 19px;
+  cursor: pointer;
+`;
+const ConfirmDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 44px;
+`;
+const Confirm = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  font-weight: lighter;
+  &:hover {
+    box-shadow: inset 2000px 0 0 0 rgba(0, 0, 0, 0.1);
+  }
+`;
+const Line = styled.img`
   height: 20px;
-  margin-right: 5px;
 `;
 
 const MessageTypeBox = ({ onChange, onSubmit, value, onKeydownChat }) => {
   const placeholder = '메세지를 입력하세요.';
   return (
     <>
-      <Wrapper onSubmit={onSubmit}>
-        <Plus src={plus} />
-        <MessageInput
-          value={value}
-          placeholder={placeholder}
-          onChange={onChange}
-          onKeyPress={onKeydownChat}
-          cols="33"
-          rows="1"
-        />
-        <SubmitButton type="submit" value="전송">
-          <img src={paper} />
-        </SubmitButton>
-      </Wrapper>
+      <Div>
+        <ConfirmDiv>
+          <Confirm>약속 확정하기</Confirm>
+          <Line src={line} />
+          <Confirm>후기 작성하기</Confirm>
+        </ConfirmDiv>
+        <TextWrapper>
+          <Form onSubmit={onSubmit}>
+            <Plus src={plus} />
+            <Wrapper>
+              <MessageInput
+                value={value}
+                placeholder={placeholder}
+                onChange={onChange}
+                onKeyPress={onKeydownChat}
+                cols="33"
+                rows="1"
+              />
+            </Wrapper>
+            <SubmitButton type="submit" value="전송">
+              <img src={paper} />
+            </SubmitButton>
+          </Form>
+        </TextWrapper>
+      </Div>
     </>
   );
 };
