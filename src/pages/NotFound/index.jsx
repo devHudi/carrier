9a19eb41918af6203a9a5683df09935293cb5c11
devHudi <devhudi@gmail.com>
2021-08-10 17:27:57 +0900

    feat: add PageCenter into not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,20 +1,8 @@
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import { WidthLimit, Flex, RoundedButton, Margin } from 'carrier-ui';
+import { RoundedButton, Margin, PageCenter } from 'carrier-ui';
 
 import illustration from './assets/illustration.svg';
-
-const Wrapper = styled(WidthLimit)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const StyledFlex = styled(Flex)`
-  height: 100%;
-`;
 
 const Illustration = styled.img`
   width: 100%;
@@ -38,31 +26,29 @@ const NotFound = () => {
   const history = useHistory();
 
   return (
-    <Wrapper>
-      <StyledFlex justify="center" align="center" direction="column">
-        <Illustration src={illustration} />
-        <Margin size={35} />
+    <PageCenter>
+      <Illustration src={illustration} />
+      <Margin size={35} />
 
-        <Title>페이지를 찾을 수 없어요.</Title>
-        <Margin size={10} />
+      <Title>페이지를 찾을 수 없어요.</Title>
+      <Margin size={10} />
 
-        <Subtitle>
-          요청하신 결과를 찾을 수 없어요.
-          <br />
-          입력하신 URL이 올바른지 확인해주세요.
-        </Subtitle>
-        <Margin size={50} />
+      <Subtitle>
+        요청하신 결과를 찾을 수 없어요.
+        <br />
+        입력하신 URL이 올바른지 확인해주세요.
+      </Subtitle>
+      <Margin size={50} />
 
-        <RoundedButton
-          blue
-          onClick={() => {
-            history.push('/');
-          }}
-        >
-          메인으로 돌아가기
-        </RoundedButton>
-      </StyledFlex>
-    </Wrapper>
+      <RoundedButton
+        blue
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        메인으로 돌아가기
+      </RoundedButton>
+    </PageCenter>
   );
 };
 
