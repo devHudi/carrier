cdce899f6bf2f5ca77d96ca4d9d59482db008a65
devHudi <devhudi@gmail.com>
2021-08-10 13:30:15 +0900

    feat: fetch data

--- src/pages/HireResultMore/components/SimilarGuide.jsx
+++ src/pages/HireResultMore/components/SimilarGuide.jsx
@@ -1,4 +1,5 @@
 import { PropTypes } from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import _ from 'lodash';
 import styled from 'styled-components';
 import { Typography, Margin, Flex } from 'carrier-ui';
@@ -8,13 +9,12 @@ import { RiHeartFill } from 'react-icons/ri';
 
 import placeData from 'assets/data/placeData';
 import themeData from 'assets/data/themeData';
-import Picture from '../data/img1.png';
 
 const Wrapper = styled.div`
   margin-bottom: 17px;
   padding: 15px;
   width: 100%;
-  background: #ffffff 0% 0% no-repeat padding-box;
+  background-color: #ffffff;
   display: flex;
   justify-content: space-between;
   border-radius: 26px;
@@ -38,8 +38,9 @@ const GuideWrapper = styled.div`
 const ImgContainer = styled.div`
   flex: 0 0 80px;
   height: 80px;
-  background: transparent url(${Picture}) 0% 0% no-repeat padding-box;
+  background-image: url(${(props) => props.image});
   background-size: cover;
+  background-position: center;
   border-radius: 112px;
   flex-grow: 0;
   flex-shrink: 0;
@@ -58,46 +59,51 @@ const Theme = styled(Typography)`
   white-space: nowrap;
 `;
 
-const SimilarGuide = ({ guides }) => (
-  <>
-    {_.map(guides, (guide) => (
-      <Wrapper>
-        <GuideWrapper>
-          <Flex justify="space-between" align="center" width="100%">
-            <ImgContainer />
-            <Flex direction="column">
-              <Flex align="flex-end">
-                <Typography headline>{guide.name}</Typography>
-                <Margin row size={10} />
-                <City subhead>
-                  {_.find(placeData, { sido: guide.place.sido }).sidoKr}
-                </City>
-              </Flex>
-              <Margin size={5} />
-              <Theme body>
-                {_.map(
-                  guide.themes,
-                  (theme) =>
-                    `# ${_.snakeCase(_.find(themeData, { id: theme }).label)}`,
-                )}
-              </Theme>
-              <Margin size={5} />
-              <Flex align="center">
-                <Typography body>
-                  <GoPerson color="blue" /> {guide.hired_count} 가이드
-                </Typography>
-                <Margin row size={10} />
-                <Typography body>
-                  <RiHeartFill color="#FF77B2" /> {guide.liked_count} like
-                </Typography>
+const SimilarGuide = ({ guides }) => {
+  const history = useHistory();
+  return (
+    <>
+      {_.map(guides, (guide) => (
+        <Wrapper onClick={() => history.push(`/profile/${guide.uid}`)}>
+          <GuideWrapper>
+            <Flex justify="space-between" align="center" width="100%">
+              <ImgContainer image={guide.profile_image} />
+              <Flex direction="column">
+                <Flex align="flex-end">
+                  <Typography headline>{guide.name}</Typography>
+                  <Margin row size={10} />
+                  <City subhead>
+                    {_.find(placeData, { sido: guide.place.sido }).sidoKr}
+                  </City>
+                </Flex>
+                <Margin size={5} />
+                <Theme body>
+                  {_.map(
+                    guide.themes,
+                    (theme) =>
+                      `# ${_.snakeCase(
+                        _.find(themeData, { id: theme }).label,
+                      )}`,
+                  )}
+                </Theme>
+                <Margin size={5} />
+                <Flex align="center">
+                  <Typography body>
+                    <GoPerson color="blue" /> {guide.hired_count} 가이드
+                  </Typography>
+                  <Margin row size={10} />
+                  <Typography body>
+                    <RiHeartFill color="#FF77B2" /> {guide.liked_count} like
+                  </Typography>
+                </Flex>
               </Flex>
             </Flex>
-          </Flex>
-        </GuideWrapper>
-      </Wrapper>
-    ))}
-  </>
-);
+          </GuideWrapper>
+        </Wrapper>
+      ))}
+    </>
+  );
+};
 
 SimilarGuide.propTypes = {
   guides: PropTypes.arrayOf(PropTypes.object).isRequired,
