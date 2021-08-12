d99f1793496f9edffe4ebbcd342b14744a6577f6
JunHyeon <dlwnsgus07@naver.com>
2021-08-12 14:08:57 +0900

    feat : add closeButton to goBack() history func

--- src/pages/RecommendResult/components/CloseButton.jsx
+++ src/pages/RecommendResult/components/CloseButton.jsx
@@ -1,10 +1,22 @@
 import { Flex } from 'carrier-ui';
 import { RiCloseLine } from 'react-icons/ri';
+import { useHistory } from 'react-router-dom';
 
-const CloseButton = () => (
-  <Flex justify="flex-end" align="center" style={{ padding: '10px' }}>
-    <RiCloseLine size="50" color="#5B5B5B" />
-  </Flex>
-);
+const CloseButton = () => {
+  const history = useHistory();
+  const click = () => {
+    history.goBack();
+  };
+  return (
+    <Flex
+      justify="flex-end"
+      align="center"
+      style={{ padding: '10px' }}
+      onClick={click}
+    >
+      <RiCloseLine size="50" color="#5B5B5B" />
+    </Flex>
+  );
+};
 
 export default CloseButton;
