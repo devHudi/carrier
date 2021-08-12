2cd292a2ed06881b213050ecf85906abba83d6ae
yeonsooSo <janu10.sys@gmail.com>
2021-08-12 18:19:52 +0900

    chore: fix layout

--- src/pages/Intro/components/Container.jsx
+++ src/pages/Intro/components/Container.jsx
@@ -1,18 +1,13 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 200px;
+  height: 28%;
   padding: 60px;
-  margin-top: 50px;
+  margin-top: 40px;
   background: transparent linear-gradient(140deg, #919cff 0%, #3b4de3 100%) 0%
     0% no-repeat padding-box;
   box-shadow: -4px -4px 15px #53535329;
   border-top-left-radius: 120px;
-  float: end;
 `;
 
 export default Container;
